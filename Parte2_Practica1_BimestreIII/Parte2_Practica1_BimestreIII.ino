216402e0d03d6fffb35f7fabf10f76965c6ebc3d
Leo-maker-2019410 <eramirez-2019410@kinal.edu.gt>
2023-06-03 19:19:40 -0600

    Parte2_Practica1_3Bim

---
+++ Parte2_Practica1_BimestreIII/Parte2_Practica1_BimestreIII.ino
@@ -0,0 +1,58 @@
+#include <Ticker.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+//Sensor de Temperatura//
+OneWire ourWire(5);
+DallasTemperature sensors(&ourWire);
+float cel;
+
+#define push_buttom 2
+
+//Funciones del ISR
+void isr_temperatura();
+void isr_buttom();
+
+//Variable
+volatile static bool estado_1;
+volatile static bool estado_2;
+
+//Constructor
+Ticker isr_accion_termometro(isr_temperatura, 3000);
+Ticker isr_accion_push(isr_buttom, 6000);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Uso de Ticker");
+  sensors.begin();
+  isr_accion_termometro.start();
+  pinMode(push_buttom, INPUT);
+  isr_accion_push.start();
+  estado_1 = LOW;
+}
+
+void loop() {
+  cel = temperaturaC();
+  isr_temperatura();
+  isr_accion_push.update();
+
+}
+
+int temperaturaC() {
+  sensors.requestTemperatures();   //Se envía el comando para leer la temperatura
+  int temp = sensors.getTempCByIndex(0); //Se obtiene la temperatura en ºF
+  return temp;
+}
+void isr_temperatura() {
+  Serial.println(cel);
+}
+
+void isr_buttom() {
+  estado_1 = digitalRead(push_buttom);
+  if (estado_1 == HIGH) {
+    Serial.println("ABIERTO");
+  }
+  if(estado_1 == LOW){
+    Serial.println("CERRADO");
+  }
+}
